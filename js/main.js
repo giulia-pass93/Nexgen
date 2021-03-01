3a1f5df7be99fee18fd8a355f20165ddd528d590
Giulia Passarino <giulia.passarino@accenture.com>
2021-03-01 19:38:48 +0100

    file oggetti

--- js/main.js
+++ js/main.js
@@ -3,31 +3,8 @@ function bodyGenerator(){
 const header=document.getElementsByClassName("navBarTop")[0];
 const navBarBottomMenu=document.getElementsByClassName("navBarBottomMenu")[0];
 
-const headerTopObject=[{ 
-    name:"Open Hours: Mon - Sat - 9:00 - 18:00",
-    url:"www."},
-  { name:"+1 (305) 1234-5678",
-    url:"www."},
-  { name:"hello@example.com",
-    url:"www."},
-  { name:"facebook",
-    url:"www."},
-  { name:"twitter",
-    url:"www."},
-  { name:"instagram",
-    url:"www."}];
-
-const mainMenuObject=[
-  { name:"HOME",
-    url:"www."},
-  { name:"SERVICES",
-    url:"www."},
-  { name:"ABOUT",
-    url:"www."},
-  { name:"PROJECTS",
-    url:"www."},
-  { name:"RESULTS",
-    url:"www."}];
+const headerTopObject = getHeaderTopObject();
+const mainMenuObject = getMainMenuObject();
 
 //navBarTop Sinistra
 const headerTopLeft=document.createElement("div");
---
+++ js/objects.js
@@ -0,0 +1,29 @@
+function getHeaderTopObject(){
+return [{ 
+    name:"Open Hours: Mon - Sat - 9:00 - 18:00",
+    url:"www."},
+  { name:"+1 (305) 1234-5678",
+    url:"www."},
+  { name:"hello@example.com",
+    url:"www."},
+  { name:"facebook",
+    url:"www."},
+  { name:"twitter",
+    url:"www."},
+  { name:"instagram",
+    url:"www."}];
+}
+
+function getMainMenuObject(){
+return [
+    { name:"HOME",
+      url:"www."},
+    { name:"SERVICES",
+      url:"www."},
+    { name:"ABOUT",
+      url:"www."},
+    { name:"PROJECTS",
+      url:"www."},
+    { name:"RESULTS",
+      url:"www."}];
+}
